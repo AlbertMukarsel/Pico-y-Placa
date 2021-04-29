1bbd8ab2219cab422633b051e9b2b4806e09d5eb
Alberto Mucarsel S <amucarsel@mag.gob.ec>
2021-04-29 12:39:24 -0500

    Fixing push to tests branch

--- picoPlaca.py
+++ picoPlaca.py
@@ -1,4 +1,23 @@
-licensePlate = input("Enter you license number plate: ")
-date = input("Enter the date you want to check: ")
-time = input("Enter the time you want to check: ")
+from inputValidations import checkInput
+from car import Car
 
+validatedLicense = False
+validatedDate = False
+validatedTime = False
+
+while(not validatedLicense):
+    licensePlate = input("Enter you license number plate: ")
+    validated=checkInput(licensePlate,"License Plate")
+
+while(not validatedDate):
+    date = input("Enter the date you want to check (format DD/MM/YYYY): ")
+    validatedDate=checkInput(date, "Date")
+
+while(not validatedTime):
+    time = input("Enter the time you want to check (format HH:mm): ")
+    validatedTime=checkInput(date,"Time")
+
+carBeenChecked=Car(licensePlate)
+carBeenChecked.hasRestriction(date, time)
+
+print("Thank you for using Pico y Placa predictor")
